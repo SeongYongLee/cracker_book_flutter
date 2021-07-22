28781862008539a9e1b0a4ea40d71f335ae57554
SY L <seongyong.dev@gmail.com>
2021-07-22 23:32:40 +0900

    MainPage 작업 중

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,33 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class Kind {
+  String title;
+  String src;
+
+  Kind(this.title, this.src);
+}
+
+class Study {
+  String title;
+
+  Study(this.title);
+}
+
 class _MyHomePageState extends State<MyHomePage> {
+  final kindList = [
+    Kind('토론', 'assets/main/chats.svg'),
+    Kind('발표', 'assets/main/microphone.svg'),
+    Kind('글쓰기', 'assets/main/note.svg'),
+    Kind('포트폴리오', 'assets/main/desktop.svg'),
+    Kind('기타', 'assets/main/etc.svg'),
+  ];
+
+  final studyList = [
+    Study('눈치껏 못 배웁니다, 일센스...'),
+    Study('생각의 쓰임'),
+    Study('엘라스틱서치 실무 가이드...')
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -59,6 +85,221 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Container(
         color: Colors.white,
+        child: SingleChildScrollView(
+          physics: ScrollPhysics(),
+          child: Column(
+            children: [
+              Container(
+                  margin:
+                  EdgeInsets.only(left: 6, right: 6, top: 10, bottom: 10),
+                  height: 100,
+                  child: ListView.builder(
+                    // 스크롤 방향 설정. 수평적으로 스크롤되도록 설정
+                      scrollDirection: Axis.horizontal,
+                      itemCount: kindList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Container(
+                              width: 70,
+                              margin: EdgeInsets.only(
+                                  left: 8, right: 8, top: 20, bottom: 20),
+                              child: SvgPicture.asset('${kindList[index].src}'),
+                            ),
+                            Container(
+                              width: 70,
+                              margin: EdgeInsets.only(left: 8, right: 8),
+                              child: Text(kindList[index].title,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.normal)),
+                            )
+                          ],
+                        );
+                      })),
+              Container(
+                margin:
+                EdgeInsets.only(left: 20, right: 20, top: 22, bottom: 10),
+                child: ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: studyList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Column(
+                                  children: [
+                                    Container(
+                                      width: 270,
+                                      child: Text(
+                                        "'${studyList[index].title}'의",
+                                        softWrap: true,
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 270,
+                                      child: Text(
+                                        "스터디",
+                                        softWrap: true,
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  "전체보기 >",
+                                  style: TextStyle(
+                                      fontSize: 14,
+                                      color: Color(0xff999999),
+                                      fontWeight: FontWeight.normal),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 20, bottom: 20),
+                            color: Colors.green,
+                            child: Row(
+                              children: <Widget>[
+                                Container(
+                                  height: 226,
+                                  child: Text(
+                                    'img area',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 20, bottom: 20),
+                            child: Row(
+                              children: <Widget>[
+                                Column(
+                                  children: [
+                                    Container(
+                                      width: 270,
+                                      child: Text(
+                                        "글쓰기 맴버 3/5",
+                                        style: TextStyle(
+                                            fontSize: 12,
+                                            color: Color(0xff677AC7),
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 270,
+                                      child: Text(
+                                        "엘라스틱서치 시스템 실습",
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 270,
+                                      child: Text(
+                                        "2021.04.26 ~ 05.21 토 14:00",
+                                        style: TextStyle(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.normal),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 20, bottom: 20),
+                            child: Row(
+                              children: <Widget>[
+                                Column(
+                                  children: [
+                                    Container(
+                                      width: 270,
+                                      child: Text(
+                                        "글쓰기 맴버 3/5",
+                                        style: TextStyle(
+                                            fontSize: 12,
+                                            color: Color(0xff677AC7),
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 270,
+                                      child: Text(
+                                        "사회초년생을 위한 직팁",
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    Container(
+                                      width: 270,
+                                      child: Text(
+                                        "2021.04.26 ~ 05.21 토 14:00",
+                                        style: TextStyle(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.normal),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      );
+                    }),
+              ),
+              Container(
+                margin:
+                EdgeInsets.only(left: 20, right: 20, top: 22, bottom: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '좋아요가 많은 스터디',
+                      style:
+                      TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin:
+                EdgeInsets.only(left: 20, right: 20, top: 22, bottom: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '새로 생긴 스터디',
+                      style:
+                      TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: Text('더 보기'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
